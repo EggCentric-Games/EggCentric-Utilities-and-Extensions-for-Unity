ad0829d010c23c709772f282b632f07b683acf79
HummeL <firemoney426@gmail.com>
2025-05-25 01:22:01 +0200

    [+] Converted EvaluationUtilities to a package

---
+++ Evaluators/Runtime/GrowthFunctions/LogarithmicGrowth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace EggCentric.Utilities.Evaluators
+{
+    public class LogarithmicGrowth : IGrowth
+    {
+        private float baseNumber;
+
+        public LogarithmicGrowth() : this(2f)
+        {
+        }
+
+        public LogarithmicGrowth(float baseNumber)
+        {
+            this.baseNumber = baseNumber;
+        }
+
+        public float Evaluate(float value)
+        {
+            return Mathf.Log(value, baseNumber);
+        }
+    }
+}
---
+++ Evaluators/Runtime/ShapingFunctions/Subclasses/OneSideShaping.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EggCentric.Utilities.Evaluators
+{
+    public class OneSideShaping : DotShaping
+    {
+        public override float GetMultiplier(float angle)
+        {
+            float baseValue = base.GetMultiplier(angle);
+
+            return (baseValue + 1f) / 2f;
+        }
+    }
+}
