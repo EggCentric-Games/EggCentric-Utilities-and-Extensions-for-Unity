0063401cefa8fc6444844b8f168bebbcf0737ec8
HummeL <firemoney426@gmail.com>
2025-05-25 09:46:53 +0200

    [+] Converted Stats implementation to a package

---
+++ Stats/CHANGELOG.md
@@ -0,0 +1,6 @@
+## [1.0.0] - 2025-05-25
+
+### First Release
+
+- Implemented IStat interface
+- Implemented bunch of base Stat implementation (simple value, limited, progressive)
---
+++ Stats/Runtime/AccumulatedStat.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace EggCentric.Stats
+{
+    public class AccumulatedStat : RangedStat
+    {
+        protected float accumulationRate;
+        protected float lossRate;
+
+        public AccumulatedStat(float min, float max, float accumulationRate, float lossRate, float value = 0) : base(min, max, value)
+        {
+            this.accumulationRate = accumulationRate;
+            this.lossRate = lossRate;
+        }
+
+        public void DoTick(float tickLength, float tickCoefficient)
+        {
+            if (tickLength <= 0 || tickCoefficient == 0)
+                return;
+
+            if (tickCoefficient > 0)
+                ChargeUp(tickLength * tickCoefficient);
+            else
+                ChargeDown(tickLength * tickCoefficient);
+        }
+
+        public void ChargeUp(float tickLength)
+        {
+            if (tickLength == 0)
+                return;
+
+            if (lossRate == Mathf.Infinity)
+                SetValue(upperLimit);
+            else
+                ChangeValue(tickLength * accumulationRate);
+        }
+
+        public void ChargeDown(float tickLength)
+        {
+            if (tickLength == 0)
+                return;
+
+            if (lossRate == Mathf.Infinity)
+                SetValue(lowerLimit);
+            else
+                ChangeValue(-(tickLength * lossRate));
+        }
+    }
+}
