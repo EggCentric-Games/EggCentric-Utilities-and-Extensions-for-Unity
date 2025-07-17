cbfd0008301ecf1617a98d8673e38fd945f60bd2
HummeL <firemoney426@gmail.com>
2025-07-17 14:02:20 +0200

    [+] Converted StateMachines to a package

---
+++ StateMachines/CHANGELOG.md
@@ -0,0 +1,7 @@
+## [1.0.0] - 2025-07-17
+
+### First Release
+
+- Implemented generic StateMachine class
+- Implemented basic state managment
+- Implemented IState and IPayloadedState
---
+++ StateMachines/Runtime/IStateMachine.cs
@@ -0,0 +1,5 @@
+﻿public interface IStateMachine<TStateType>
+{
+    void Enter<TState>() where TState : class, TStateType, IState;
+    void Enter<TState, TPayload>(TPayload payload) where TState : class, TStateType, IPayloadedState<TPayload>;
+}
