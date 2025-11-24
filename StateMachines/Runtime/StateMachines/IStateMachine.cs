d8abd28928a3d51d50df3b020e18ffdfd45f58af
HummeL <firemoney426@gmail.com>
2025-11-24 11:04:30 +0100

    [*] Remove unused code

--- StateMachines/Runtime/StateMachines/IStateMachine.cs
+++ StateMachines/Runtime/StateMachines/IStateMachine.cs
@@ -8,7 +8,5 @@
         public ITransitionBuilder To<TTarget, TPayload>(TPayload payload) where TTarget : class, IPayloadedState<TPayload>, TStateType;
         public void ExecuteTransition<TTarget>(ITransition<TTarget> transition) where TTarget : class, ICommonState, TStateType;
         public void ExecuteTransition<TTarget, TPayload>(ITransition<TTarget> transition, TPayload payload) where TTarget : class, IPayloadedState<TPayload>, TStateType;
-        //void Enter<TState>() where TState : class, TStateType, ICommonState;
-        //void Enter<TState, TPayload>(TPayload payload) where TState : class, TStateType, IPayloadedState<TPayload>;
     }
 }
