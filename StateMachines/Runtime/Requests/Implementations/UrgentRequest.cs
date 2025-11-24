3d480d116c21a0e6e348132f2eb6c38249ab7133
HummeL <firemoney426@gmail.com>
2025-11-24 20:01:11 +0100

    [*] Fixed UrgentRequest lifetime

--- StateMachines/Runtime/Requests/Implementations/UrgentRequest.cs
+++ StateMachines/Runtime/Requests/Implementations/UrgentRequest.cs
@@ -4,7 +4,16 @@ namespace EggCentric.StateMachines
 {
     public class UrgentRequest : TransitionRequest
     {
-        public override bool IsValid => true;
+        public override bool IsValid
+        {
+            get {
+                bool isValid = !_isConsumed;
+                _isConsumed = true;
+                return isValid;
+            }
+        }
+
+        private bool _isConsumed = false;
 
         public UrgentRequest(Type targetState, bool isForced = false, int priority = 0) : base(targetState, priority)
         {
