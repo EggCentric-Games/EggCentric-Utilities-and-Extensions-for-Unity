4cf5107e368983a10980915898c025fcaead0d84
HummeL <firemoney426@gmail.com>
2025-11-26 12:01:17 +0100

    [+] Implemented StopCoroutine support

--- Infrastructure/Runtime/GameObjectProxy/CoroutineRunning/ICoroutineRunner.cs
+++ Infrastructure/Runtime/GameObjectProxy/CoroutineRunning/ICoroutineRunner.cs
@@ -5,6 +5,8 @@ namespace EggCentric.Infrastructure
 {
     public interface ICoroutineRunner : IService
     {
-        public Coroutine StartCoroutine(IEnumerator enumerator);
+        public Coroutine StartCoroutine(IEnumerator routine);
+        public void StopCoroutine(Coroutine routine);
+        public void StopCoroutine(IEnumerator routine);
     }
 }
