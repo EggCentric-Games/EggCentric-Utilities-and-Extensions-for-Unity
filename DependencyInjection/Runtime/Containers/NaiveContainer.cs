7d71d7188e4525046ad8528ad5d940d9ac7229b7
HummeL <firemoney426@gmail.com>
2025-12-29 13:29:15 +0100

    [+] Implemented typeless NaiveContainer

--- DependencyInjection/Runtime/Containers/NaiveContainer.cs
+++ DependencyInjection/Runtime/Containers/NaiveContainer.cs
@@ -4,6 +4,52 @@ using UnityEngine;
 
 namespace EggCentric.DI
 {
+    public class NaiveContainer : IContainer
+    {
+        private readonly Dictionary<Type, object> _bindings;
+
+        public NaiveContainer() => _bindings = new Dictionary<Type, object>();
+
+        public void Bind(object service)
+        {
+            if (_bindings.TryGetValue(service.GetType(), out var binding))
+            {
+                Debug.LogWarning($"Service {service.GetType()} was binded already");
+                return;
+            }
+
+            _bindings.Add(service.GetType(), service);
+        }
+
+        public bool Resolve<TRequested>(out TRequested service)
+        {
+            service = default;
+
+            if (!_bindings.TryGetValue(typeof(TRequested), out var binding))
+            {
+                Debug.LogWarning($"There is no registered service of type {typeof(TRequested)}");
+                return false;
+            }
+
+            service = (TRequested)binding;
+            return true;
+        }
+
+        public bool ResolveAll<TRequested>(out TRequested[] services)
+        {
+            services = Array.Empty<TRequested>();
+
+            if (!_bindings.TryGetValue(typeof(TRequested), out var binding))
+            {
+                Debug.LogWarning($"There is no registered service of type {typeof(TRequested)}");
+                return false;
+            }
+
+            services = new TRequested[] { (TRequested)binding };
+            return true;
+        }
+    }
+
     public class NaiveContainer<TItem> : IContainer<TItem>
     {
         private readonly Dictionary<Type, TItem> _bindings;
